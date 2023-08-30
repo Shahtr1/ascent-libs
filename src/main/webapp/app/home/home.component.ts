0d7fcd691c4a77514bbf4857a047905d62213614
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2023-08-31 01:30:22 +0530

    Added base table package

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { TableInputOptions, TableViewConfig } from 'asc-shared-libs';
+import { SortEvent } from 'primeng/api';
+import * as _ from 'lodash-es';
 
-import { LoginService } from 'app/login/login.service';
-import { AccountService } from 'app/core/auth/account.service';
-import { Account } from 'app/core/auth/account.model';
+type AccountPurpose = {
+  refId: string;
+  shortName: string;
+  middleName: string;
+  fullName: string;
+  trxnStatus: string;
+  isActive: boolean;
+  referenceName?: string;
+};
 
 @Component({
   selector: 'jhi-home',
@@ -10,15 +19,102 @@ import { Account } from 'app/core/auth/account.model';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  account: Account | null = null;
+  tableInputOptions!: TableInputOptions<AccountPurpose>;
 
-  constructor(private accountService: AccountService, private loginService: LoginService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.accountService.identity().subscribe(account => (this.account = account));
+  accountPurposes: AccountPurpose[] = [];
+
+  selectedAccountPurposes: AccountPurpose[] = [];
+
+  ngOnInit() {
+    this.accountPurposes = this.getAccountPurposeData();
+
+    this.setTableInputOptions();
   }
 
-  login(): void {
-    this.loginService.login();
+  private setTableInputOptions() {
+    this.tableInputOptions = new TableInputOptions<AccountPurpose>({
+      customSort: false,
+      customSortFn: this.customSortFn.bind(this),
+      selectionMode: 'multiple',
+      rowExpand: true,
+      scrollHeight: 370,
+      exportTypes: ['pdf', 'csv', 'excel'],
+      selectionPageOnly: true,
+      modifyConfigFn: this.modifyConfigFn.bind(this),
+      transformDataFn: this.transformDataFn.bind(this),
+      exportFileName: 'account-purposes',
+      globalSearch: true,
+      showAddButton: true,
+      rowsSelectionDisabled: false,
+    });
+  }
+
+  private getAccountPurposeData(): AccountPurpose[] {
+    let testAccountPurposeData = [];
+    for (let i = 0; i < 100; i++) {
+      testAccountPurposeData.push({
+        refId: 'refId' + i,
+        shortName: 'shortName' + i,
+        middleName: 'middleName' + i,
+        fullName: 'fullName' + i,
+        trxnStatus: _.random(0, 1) === 1 ? 'Completed' : 'Failed',
+        isActive: _.random(0, 1) === 1,
+      });
+    }
+
+    return testAccountPurposeData;
+  }
+
+  modifyConfigFn(config: TableViewConfig): TableViewConfig {
+    config.columns = [
+      ...config.columns,
+      {
+        field: 'referenceName',
+        headerId: 'L_REFERENCE_NAME',
+        filter: 'text',
+        input: {
+          type: 'number',
+          placeholderId: 'L_ENTER_REFERENCE_NAME',
+          textConfig: {
+            icon: 'pi pi-camera',
+            position: 'right',
+          },
+        },
+      },
+      {
+        field: 'isActive',
+        headerId: 'L_IS_ACTIVE',
+        filter: 'boolean',
+      },
+    ];
+
+    config.settings.transformData = true;
+    config.settings.export = true;
+    config.settings.showAddButton = false;
+    config.settings.globalSearch = true;
+    config.settings.firstColumnFrozen = true;
+
+    return config;
+  }
+
+  transformDataFn(data: AccountPurpose[]): AccountPurpose[] {
+    data.forEach(item => {
+      item.referenceName = item.shortName + ' ' + item.middleName;
+    });
+    return data;
+  }
+
+  customSortFn: (event: SortEvent) => number = (event: SortEvent) => {
+    return 1;
+  };
+
+  onRowUnselect(data: AccountPurpose) {
+    console.log('onRowUnselect', data);
+  }
+
+  onRowSelect(data: AccountPurpose) {
+    console.log('onRowSelect', data);
   }
 }
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,13 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { TableModule } from 'primeng/table';
+import { AscSharedLibsModule } from 'asc-shared-libs';
+import { environment } from '../environment';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), TableModule, AscSharedLibsModule],
   declarations: [HomeComponent],
+  providers: [{ provide: 'environment', useValue: environment }],
 })
 export class HomeModule {}
