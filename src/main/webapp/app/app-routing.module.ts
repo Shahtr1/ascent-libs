11beb5785bd2814bf53b1d596a5265edbee58d80
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2023-09-01 02:21:36 +0530

    Fixed display of app

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -3,32 +3,30 @@ import { RouterModule } from '@angular/router';
 
 import { errorRoute } from './layouts/error/error.route';
 import { navbarRoute } from './layouts/navbar/navbar.route';
-import { DEBUG_INFO_ENABLED } from 'app/app.constants';
 import { Authority } from 'app/config/authority.constants';
 
 import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
+import { EntityRoutingModule } from './entities/entity-routing.module';
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(
-      [
-        {
-          path: 'admin',
-          data: {
-            authorities: [Authority.ADMIN],
-          },
-          canActivate: [UserRouteAccessService],
-          loadChildren: () => import('./admin/admin-routing.module').then(m => m.AdminRoutingModule),
+    EntityRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: 'admin',
+        data: {
+          authorities: [Authority.ADMIN],
         },
-        {
-          path: '',
-          loadChildren: () => import(`./entities/entity-routing.module`).then(m => m.EntityRoutingModule),
-        },
-        navbarRoute,
-        ...errorRoute,
-      ],
-      { enableTracing: DEBUG_INFO_ENABLED }
-    ),
+        canActivate: [UserRouteAccessService],
+        loadChildren: () => import('./admin/admin-routing.module').then(m => m.AdminRoutingModule),
+      },
+      {
+        path: '',
+        loadChildren: () => import(`./entities/entity-routing.module`).then(m => m.EntityRoutingModule),
+      },
+      navbarRoute,
+      ...errorRoute,
+    ]),
   ],
   exports: [RouterModule],
 })
